82563b92ced6400d69a280dbeb92b0230a41a0a5
Luca Garbelli <lu.gbelli@hotmail.it>
2018-01-16 11:53:56 +0100

    - aggiunti script del sistema di pool, non usati ora

---
+++ Assets/Scripts/PoolSystem/GameObjectPool.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Framework.PoolSystem
+{
+    public class GameObjectPool : Pool<GameObject>
+    {
+        public GameObjectPool(GameObject _prefabReference, Vector3 _containerObj, Transform _containerParent)
+        {
+            InitializeObjectPool(_prefabReference, _containerObj, _containerParent, 0);
+        }
+
+        public GameObjectPool(GameObject _prefabReference, Vector3 _containerObj, Transform _containerParent, int _initialQuantity)
+        {
+            InitializeObjectPool(_prefabReference, _containerObj, _containerParent, _initialQuantity);
+        }
+
+        protected override GameObject GetPoolType(GameObject gameObject)
+        {
+            return gameObject;
+        }
+
+        protected override void ResetPool(GameObject _item)
+        {
+            _item.transform.position = parentObject.transform.position;
+            _item.transform.parent = parentObject.transform;
+        }
+
+        protected override void ChangeObjectState(GameObject item, bool toState)
+        {
+            item.SetActive(toState);
+        }
+
+        protected override bool IsObjectActive(GameObject item)
+        {
+            return item.activeSelf;
+        }
+    }
+}
---
+++ Assets/Scripts/PoolSystem/ObjectPool.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Framework.PoolSystem
+{
+    public class ObjectPool<PoolType> : Pool<PoolType> where PoolType : MonoBehaviour
+    {
+        public ObjectPool(GameObject _prefabReference, Vector3 _containerObj, Transform _containerParent)
+        {
+            InitializeObjectPool(_prefabReference, _containerObj, _containerParent, 0);
+        }
+
+        public ObjectPool(GameObject _prefabReference, Vector3 _containerObj, Transform _containerParent, int _initialQuantity)
+        {
+            InitializeObjectPool(_prefabReference, _containerObj, _containerParent, _initialQuantity);
+        }
+
+        protected override PoolType GetPoolType(GameObject gameObject)
+        {
+            PoolType type = gameObject.GetComponent<PoolType>();
+            return type;
+        }
+
+        protected override void ResetPool(PoolType _item)
+        {
+            _item.transform.position = parentObject.transform.position;
+            _item.transform.parent = parentObject.transform;
+        }
+
+        protected override void ChangeObjectState(PoolType item, bool toState)
+        {
+            item.gameObject.SetActive(toState);
+        }
+
+        protected override bool IsObjectActive(PoolType item)
+        {
+            return item.gameObject.activeSelf;
+        }
+    }
+}
