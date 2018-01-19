4f3f62e495b76f156201035a55569c46a54a5083
Luca Garbelli <lu.gbelli@hotmail.it>
2018-01-19 11:31:22 +0100

    - minor fix

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,22 +10,18 @@ public class Movement : MonoBehaviour {
 
     public void Move(Vector3 _position)
     {
-        transform.position = Vector3.Lerp(transform.position, transform.position + _position, MovementSpeed);
+        transform.position = Vector3.Lerp(transform.position, transform.position + _position, MovementSpeed * Time.deltaTime);
     }
 
     public void Rotate()
     {
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         RaycastHit floorHit;
 
         if (Physics.Raycast(mouseRay, out floorHit))
         {
             Vector3 playerToMouse = floorHit.point - transform.position;
-
             playerToMouse.y = 0;
-
-            //rigid.MoveRotation(Quaternion.LookRotation(playerToMouse));
             ModelToRotate.transform.rotation = Quaternion.LookRotation(playerToMouse);
         }
     }
