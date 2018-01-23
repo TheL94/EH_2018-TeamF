303354426371095b2c9d5fcf27aa24372b2e5dbd
FlyingBoar <montecristo310@gmail.com>
2018-01-23 20:53:50 +0100

    - Aggiunte ammo sulla weapon - Aggiunta ammo crate per ricaricare i proiettili - Aggiunta cadenza di sparo e mantenendo cliccato il mouse spara in base a questa cadenza

---
+++ Assets/Scripts/AmmoCrate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCrate : MonoBehaviour {
+
+    public int Ammo;
+
+    public void DestroyAmmoCrate()
+    {
+        Destroy(gameObject);
+    }
+
+}
