57f47550cf49a05c92de8875203aa1d6d119c31e
FlyingBoar <montecristo310@gmail.com>
2018-02-20 17:01:11 +0100

    Spostati gli scriptable in file separati

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -170,20 +170,4 @@ namespace TeamF
             enemiesSpawned.Clear();
         }
     }
-
-    [CreateAssetMenu(fileName = "SpawnerData", menuName = "EnemySpawner/SpawnerData", order = 1)]
-    public class EnemySpawnerData : ScriptableObject
-    {
-        public float StartDelayTime;
-        public float DelayHordes;
-
-        public int MaxHordeNumber;
-        public int MinHordeNumber;
-
-        public int MaxElementalsEnemies;
-        public int MinElementalsEnemies;
-
-        public List<EnemyData> EnemiesData;
-    }
-
 }
