da21287deb22445e98bf24ea0204531f431214e7
FlyingBoar <montecristo310@gmail.com>
2018-02-23 16:19:45 +0100

    Spostato moltiplicatore del danno nel character

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -65,6 +65,7 @@ namespace TeamF
         /// <param name="_type">Tipo del nemico che attacca, per triggherare azioni particolari del player a seconda del tipo di nemico</param>
         public void TakeDamage(float _damage, ElementalType _type = ElementalType.None)
         {
+            _damage += _damage * DamageMultiplier;
             Life -= _damage;
             if (Life <= 0)
             {
