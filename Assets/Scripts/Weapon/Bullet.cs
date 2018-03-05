ab4ca5c735b70db4bc0af209b2e4dcec6a27847f
Luca Garbelli <lu.gbelli@hotmail.it>
2018-03-05 15:38:43 +0100

    Minor fix

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -30,45 +30,65 @@ namespace TeamF
             ammo = _currentAmmo;
             Speed = _speed;
             owner = _owner;
-            //trail = GetComponentInChildren<TrailRenderer>();
-            //rend = GetComponentInChildren<MeshRenderer>();
+            trail = GetComponentInChildren<TrailRenderer>();
+            rend = GetComponentInChildren<MeshRenderer>();
 
-            //SetBulletColors(_currentAmmo.AmmoType);
+            SetBulletColors(_currentAmmo.AmmoType);
         }
 
         #endregion
 
-        //void SetBulletColors(ElementalType _type)
-        //{
-        //    switch (_type)
-        //    {
-        //        case ElementalType.None:
-        //            //rend.material.color = Color.grey;
-        //            //trail.material.color = Color.grey;
-        //            //trail.material.SetColor("_EmissionColor", Color.grey);
-        //            break;
-        //        case ElementalType.Fire:
-        //            //rend.material.color = Color.red;
-        //            //trail.material.color = Color.red;
-        //            //trail.material.SetColor("_EmissionColor", Color.red);
-        //            break;
-        //        case ElementalType.Water:
-        //            rend.material.color = Color.blue;
-        //            //trail.material.color = Color.blue;
-        //            //trail.material.SetColor("_EmissionColor", Color.blue);
-        //            break;
-        //        case ElementalType.Poison:
-        //            rend.material.color = Color.green;
-        //            //trail.material.color = Color.green;
-        //            //trail.material.SetColor("_EmissionColor", Color.green);
-        //            break;
-        //        case ElementalType.Thunder:
-        //            rend.material.color = Color.magenta;
-        //            //trail.material.color = Color.magenta;
-        //            //trail.material.SetColor("_EmissionColor", Color.magenta);
-        //            break;
-        //    }
-        //}
+        void SetBulletColors(ElementalType _type)
+        {
+            switch (_type)
+            {
+                case ElementalType.None:
+                    if(rend != null)
+                        rend.material.color = Color.grey;
+                    if(trail != null)
+                    {
+                        trail.material.color = Color.grey;
+                        trail.material.SetColor("_EmissionColor", Color.grey);
+                    }
+                    break;
+                case ElementalType.Fire:
+                    if (rend != null)
+                        rend.material.color = Color.red;
+                    if (trail != null)
+                    {
+                        trail.material.color = Color.red;
+                        trail.material.SetColor("_EmissionColor", Color.red);
+                    }           
+                    break;
+                case ElementalType.Water:
+                    if (rend != null)
+                        rend.material.color = Color.blue;
+                    if (trail != null)
+                    {
+                        trail.material.color = Color.blue;
+                        trail.material.SetColor("_EmissionColor", Color.blue);
+                    }
+                    break;
+                case ElementalType.Poison:
+                    if (rend != null)
+                        rend.material.color = Color.green;
+                    if (trail != null)
+                    {
+                        trail.material.color = Color.green;
+                        trail.material.SetColor("_EmissionColor", Color.green);
+                    }
+                    break;
+                case ElementalType.Thunder:
+                    if (rend != null)
+                        rend.material.color = Color.magenta;
+                    if (trail != null)
+                    {
+                        trail.material.color = Color.magenta;
+                        trail.material.SetColor("_EmissionColor", Color.magenta);
+                    }
+                    break;
+            }
+        }
 
         void Move()
         {
