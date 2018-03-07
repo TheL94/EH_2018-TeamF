1514a666a4b39ae1b13c273e62bbc949678eabd2
Luca Garbelli <lu.gbelli@hotmail.it>
2018-03-07 12:54:52 +0100

    Merge fix

--- Assets/Scripts/Scena di test/EnemySpawner_TS.cs
+++ Assets/Scripts/Scena di test/EnemySpawner_TS.cs
@@ -27,7 +27,7 @@ namespace TeamF
         {
             for (int i = 0; i < SpawnPoints.Count; i++)
             {
-                SpawnEnemy(SpawnPoints[i], ManichiniDiDestinazione[i], (ElementalType)i + 1);
+                SpawnEnemy(SpawnPoints[i], ManichiniDiDestinazione[i], (EnemyType)i + 2);
             }
         }
 
@@ -37,19 +37,19 @@ namespace TeamF
         /// <param name="_spawner">Lo spawn point dove deve essere spawnatp</param>
         /// <param name="_dummy">Il manichino da attaccare, se non deve seguire il player ma andare in un punto della mappa</param>
         /// <param name="_type">Il Tipo del nemico</param>
-        void SpawnEnemy(Transform _spawner, IDamageable _dummy, ElementalType _type)
+        void SpawnEnemy(Transform _spawner, IDamageable _dummy, EnemyType _type)
         {
             Enemy _newEnemy = SpawnEnemy(EnemyPrefab, _spawner);
             if (!FollowPlayer)
-                _newEnemy.Init(_dummy, FindEnemyDataByTypeAndElement(EnemyType.Melee, _type), Data.EnemyInitialState, "Enemy" + idCounter);
+                _newEnemy.Init(_dummy, FindEnemyDataByType(_type), Data.EnemyInitialState, "Enemy" + idCounter);
             else
-                _newEnemy.Init(EnemyTarget, FindEnemyDataByTypeAndElement(EnemyType.Melee, _type), Data.EnemyInitialState, "Enemy" + idCounter);
+                _newEnemy.Init(EnemyTarget, FindEnemyDataByType(_type), Data.EnemyInitialState, "Enemy" + idCounter);
         }
 
         public override void OnEnemyDeath(Enemy _enemyKilled)
         {
             Destroy(_enemyKilled.gameObject);
-            SpawnEnemy(SpawnPoints[(int)_enemyKilled.Data.ElementalType - 1], ManichiniDiDestinazione[(int)_enemyKilled.Data.ElementalType - 1], _enemyKilled.Data.ElementalType);
+            SpawnEnemy(SpawnPoints[(int)_enemyKilled.Data.ElementalType - 1], ManichiniDiDestinazione[(int)_enemyKilled.Data.ElementalType - 1], _enemyKilled.Data.EnemyType);
         }
     }
 }
