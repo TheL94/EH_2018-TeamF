f98097e132427c00eb21868275e818989b8a6cff
Luca <lu.gbelli@hotmail.it>
2018-03-30 13:14:43 +0200

    pAttern del nemico di fuoco funzionante

--- Assets/Scripts/Enemy/AI_Implementation/FireEnemyActions/Enemy_Fire_CalculateRangedAttackPosition.cs
+++ Assets/Scripts/Enemy/AI_Implementation/FireEnemyActions/Enemy_Fire_CalculateRangedAttackPosition.cs
@@ -8,17 +8,17 @@ namespace TeamF.AI
     {
         protected override bool Act(AI_Controller _controller)
         {
-            EvaluateRangedAttackPosition((_controller as AI_Enemy).Enemy);
+            CalculateRangedAttackPosition((_controller as AI_Enemy).Enemy);
             return true;
         }
 
-        void EvaluateRangedAttackPosition(Enemy _enemy)
+        void CalculateRangedAttackPosition(Enemy _enemy)
         {
             if (!_enemy.AI_Enemy.FireIsDisengaging) 
             {
                 _enemy.AI_Enemy.FireIsDisengaging = true;
                    Vector3 disengageDestination = (_enemy.Target.Position - _enemy.Position).normalized;
-                disengageDestination *= _enemy.Data.RangedDamageRange + 0.5f;
+                disengageDestination *= _enemy.Data.RangedDamageRange + 0.25f;
                 _enemy.Agent.destination = _enemy.Target.Position + disengageDestination;
             }
         }
