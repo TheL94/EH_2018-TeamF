aefb45a46cecdcfaecc28febb4a1c5265838b973
Luca <lu.gbelli@hotmail.it>
2018-04-04 19:18:50 +0200

    Aggiunto pattern del menico di veleno, non testato

---
+++ Assets/Scripts/Enemy/AI_Implementation/PoisonEnemyActions/Enemy_Poison_ReleaseCloud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFramework.AI;
+
+namespace TeamF.AI
+{
+    [CreateAssetMenu(menuName = "AI/NewAction/Enemy_Poison_ReleaseCloud")]
+    public class Enemy_Poison_ReleaseCloud : AI_Action
+    {
+        protected override bool Act(AI_Controller _controller)
+        {
+            ReleaseCloud((_controller as AI_Enemy).Enemy);
+            return true;
+        }
+
+        void ReleaseCloud(Enemy _enemy)
+        {
+            if (!_enemy.AI_Enemy.IsDisengaging)
+            {
+                EnemyPoisonData data = _enemy.Data as EnemyPoisonData;
+                GameObject.Instantiate(data.CloudPrefab, _enemy.transform.position, Quaternion.identity, null);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Datas/EnemyPoisonData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TeamF
+{
+    [CreateAssetMenu(fileName = "EnemyPoisonData", menuName = "Enemy/EnemyPoisonData")]
+    public class EnemyPoisonData : EnemyGenericData
+    {
+        [Header("Poison Specific Parameters")]
+        public GameObject CloudPrefab;
+        public float CloudReleaseDistance;
+    }
+}
