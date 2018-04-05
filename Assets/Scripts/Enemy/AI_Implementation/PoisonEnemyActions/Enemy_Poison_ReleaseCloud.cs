99ba2a1cb81662fbacc6d8ec8d31690ccc27d748
Luca <lu.gbelli@hotmail.it>
2018-04-05 12:07:37 +0200

    Minor fix

--- Assets/Scripts/Enemy/AI_Implementation/PoisonEnemyActions/Enemy_Poison_ReleaseCloud.cs
+++ Assets/Scripts/Enemy/AI_Implementation/PoisonEnemyActions/Enemy_Poison_ReleaseCloud.cs
@@ -19,7 +19,7 @@ namespace TeamF.AI
             if (!_enemy.AI_Enemy.IsDisengaging)
             {
                 EnemyPoisonData data = _enemy.Data as EnemyPoisonData;
-                GameObject cloud = GameObject.Instantiate(data.CloudPrefab, _enemy.transform.position, Quaternion.identity, null);
+                GameObject cloud = GameObject.Instantiate(data.CloudPrefab, _enemy.transform.position + (Vector3.up * 2), Quaternion.identity, null);
                 _enemy.AI_Enemy.StartObscuringCloudLifeTimeCountlDown(data.CloudLifeTime, cloud);
             }
         }
