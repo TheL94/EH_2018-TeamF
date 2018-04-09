4ba4afe0a075b5a97bbb7187e16c7503297dbb1b
FlyingBoar <montecristo310@gmail.com>
2018-04-09 12:39:35 +0200

    Fix Dash

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -16,9 +16,17 @@ namespace TeamF
         public void ActivateDash(Vector3 _direction)
         {
             //Vector3 newPos = movement.ModelToRotate.transform.forward;  //transform.worldToLocalMatrix.MultiplyVector(transform.forward); ;
-            _direction *= 20;
-
+            _direction *= 35;
             movement.Move(_direction);
+
+
+            if(_direction.x == 0 && _direction.z == 0)
+            {
+                Vector3 newPos = movement.ModelToRotate.transform.forward;  //transform.worldToLocalMatrix.MultiplyVector(transform.forward); 
+                newPos *= 35;
+                movement.Move(newPos);
+            }
+
         }
     }
 }
