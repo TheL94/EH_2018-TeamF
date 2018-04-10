356970559580055d4537175df55c76e5847b3780
FlyingBoar <montecristo310@gmail.com>
2018-04-10 10:57:52 +0200

    - Minor Fix

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -15,14 +15,13 @@ namespace TeamF
 
         public void ActivateDash(Vector3 _direction)
         {
-            //Vector3 newPos = movement.ModelToRotate.transform.forward;  //transform.worldToLocalMatrix.MultiplyVector(transform.forward); ;
             _direction *= 35;
             movement.Move(_direction);
 
 
             if(_direction.x == 0 && _direction.z == 0)
             {
-                Vector3 newPos = movement.ModelToRotate.transform.forward;  //transform.worldToLocalMatrix.MultiplyVector(transform.forward); 
+                Vector3 newPos = movement.ModelToRotate.transform.forward;
                 newPos *= 35;
                 movement.Move(newPos);
             }
