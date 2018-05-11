e2b9afb2136a7bfda7e55e0617969ff44e65e637
Luca <lu.gbelli@hotmail.it>
2018-05-11 12:01:03 +0200

    Corretta la nuvola di veleno

---
+++ Assets/Scripts/Misc/PoisonCloud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamF
+{
+    public class PoisonCloud : MonoBehaviour
+    {
+        public void Init(float _lifeTime)
+        {
+            StartCoroutine(ObscuringCloudLifeTime(_lifeTime, gameObject));
+        }
+
+        IEnumerator ObscuringCloudLifeTime(float _lifeTime, GameObject _cloud)
+        {
+            yield return new WaitForSeconds(_lifeTime);
+            DestroyObject(_cloud);
+        }
+    }
+}
+
