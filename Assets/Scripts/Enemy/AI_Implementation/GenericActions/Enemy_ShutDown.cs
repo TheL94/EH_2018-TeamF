2d8f78933ca0ab9bad1e1a8b8d9aa34d90ca43bf
Luca Garbelli <lu.gbelli@hotmail.it>
2018-05-21 15:13:34 +0200

    Aggiunta azione di shut down

---
+++ Assets/Scripts/Enemy/AI_Implementation/GenericActions/Enemy_ShutDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFramework.AI;
+
+namespace TeamF.AI
+{
+    [CreateAssetMenu(menuName = "AI/NewAction/Enemy_ShutDown")]
+    public class Enemy_ShutDown : AI_Action
+    {
+        protected override bool Act(AI_Controller _controller)
+        {
+            ShutDown((_controller as AI_Enemy).Enemy);
+            return true;
+        }
+
+        void ShutDown(Enemy _enemy)
+        {
+            _enemy.AI_Enemy.IsActive = false;
+            _enemy.GetComponent<Collider>().enabled = false;
+
+            EffectController effect = _enemy.GetComponent<EffectController>();
+            if (effect != null)
+                effect.gameObject.SetActive(false);
+
+            _enemy.GetComponentInChildren<ParticlesController>().StopAllParticles();
+            _enemy.GetComponentInChildren<BlinkController>().ResetEffects();
+        }
+    }
+}
