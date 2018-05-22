c38c31e403a8aa1bb9331dcfebf8307e35237965
Luca Garbelli <lu.gbelli@hotmail.it>
2018-05-22 11:26:25 +0200

    Aggiunte icone deile casse dei proiettili

--- Assets/Scripts/AmmoCrates/AmmoCratesController.cs
+++ Assets/Scripts/AmmoCrates/AmmoCratesController.cs
@@ -40,6 +40,9 @@ namespace TeamF
         /// <param name="_crate">La Crate che deve essere rimossa</param>
         public void DeleteAmmoCrateFromList(AmmoCrate _crate)
         {
+            _crate.Graphic.SetActive(false);
+            GameManager.I.PoolMng.UpdatePool(_crate.CurrentGraphicID);
+
             Crates.Remove(_crate);
             Destroy(_crate.gameObject);
 
