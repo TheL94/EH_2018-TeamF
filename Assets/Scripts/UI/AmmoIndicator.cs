41c00c96b71be553e8002b33b7883db42931a962
Fulvio <montecristo310@gmail.com>
2018-05-30 15:05:19 +0200

    - Modifiche alla UI dei proiettili per integrarla con la ruota delle munizioni (non ancora implementato in quanto non è presente la grafica)

---
+++ Assets/Scripts/UI/AmmoIndicator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TeamF
+{
+    public class AmmoIndicator : MonoBehaviour
+    {
+        public Text AmmoText;
+        public Image SelectedImage;
+
+        /// <summary>
+        /// Attiva o disattiva l'immagine per indicare se la munizione è selezionata
+        /// </summary>
+        /// <param name="_isActive">Lo stato che deve avere</param>
+        public void SetEnabled(bool _isActive)
+        {
+            if (_isActive)
+                SelectedImage.enabled = true;
+            else
+                SelectedImage.enabled = false;
+        }
+
+        /// <summary>
+        /// Scrive la quantità di munizioni
+        /// </summary>
+        /// <param name="_ammoCount">La quantità di munizioni</param>
+        public void SetAmmoCount(string _ammoCount)
+        {
+            AmmoText.text = _ammoCount;
+        }
+    }
+}
