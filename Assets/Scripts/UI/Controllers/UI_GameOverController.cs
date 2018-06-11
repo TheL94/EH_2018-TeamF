523f39f8d19fd287dbf80ee2a464474c632f2e22
Luca Garbelli <lu.gbelli@hotmail.it>
2018-06-11 15:53:23 +0200

    Aggiunto best score alla schermata di fine round

--- Assets/Scripts/UI/Controllers/UI_GameOverController.cs
+++ Assets/Scripts/UI/Controllers/UI_GameOverController.cs
@@ -13,6 +13,7 @@ namespace TeamF
 
         public Text PartialScore;
         public Text TotalScore;
+        public Text BestScore;
 
         public void Init(LevelEndingStaus _levelStaus)
         {
@@ -39,6 +40,7 @@ namespace TeamF
 
             PartialScore.text = GameManager.I.ScoreCounter.LastPartialScore.ToString();
             TotalScore.text = GameManager.I.ScoreCounter.TotalScore.ToString();
+            BestScore.text = GameManager.I.ScoreCounter.BestScore.ToString();
         }
 
         public override void Select()
